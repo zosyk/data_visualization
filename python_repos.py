329edbe0f2d5cdd721836823f6c61b1e28c37558
zosyk <alekcandrzosyk@gmail.com>
2018-04-21 19:39:59 +0300

    created the most starred python repos chart

---
+++ python_repos.py
@@ -0,0 +1,38 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# Make an API call and store the response
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+response = requests.get(url)
+
+response_dict = response.json()
+
+print("Total repositories:", response_dict.get("total_count"))
+
+repo_dicts = response_dict["items"]
+print("Returned repositories count:", len(repo_dicts))
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('output/python_repos.svg')
